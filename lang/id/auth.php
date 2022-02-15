e90fb28a560fac93163c2697c3b513289d7cc6fc
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2022-02-15 17:47:36 +0700

    add: feat login

---
+++ lang/id/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Informasi yang anda berikan tidak benar.',
+    'password' => 'Kata sandi salah.',
+    'throttle' => 'Terlalu banyak percobaan, coba lagi dalam :seconds detik.',
+
+];
--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -7,15 +7,18 @@
             <div class="text-center text-md-center mb-4 mt-md-0">
                 <h1 class="mb-0 h3">Masuk ke platform kami</h1>
             </div>
-            <form action="#" class="mt-4">
-                <!-- Form -->
+            <form action="{{ route('admin.login') }}" class="mt-4" method="post">
+                @csrf
                 <div class="form-group mb-4">
                     <label for="email">Email Anda</label>
                     <div class="input-group">
                         <span class="input-group-text" id="basic-addon1">
                             <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
                         </span>
-                        <input type="email" class="form-control" placeholder="example@company.com" id="email" name="email" autofocus>
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="example@company.com" id="email" name="email" value="{{ old('email') }}" autofocus>
+                        @error('email')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>  
                 </div>
                 <!-- End of Form -->
@@ -27,13 +30,17 @@
                             <span class="input-group-text" id="basic-addon2">
                                 <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path></svg>
                             </span>
-                            <input type="password" placeholder="Password" class="form-control" id="password" name="password">
+                            <input type="password" placeholder="Password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
+
+                            @error('password')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
                         </div>  
                     </div>
                     <!-- End of Form -->
                     <div class="d-flex justify-content-between align-items-top mb-4">
                         <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id="remember">
+                            <input class="form-check-input" type="checkbox" id="remember" name="remember" @checked(old('remember'))>
                             <label class="form-check-label mb-0" for="remember">
                               Ingat saya
                             </label>
