4d3e5944a1a68c474c39b9db6c05a2a2fc8d8f0f
destroylord <masapin68@gmail.com>
2022-02-17 10:23:31 +0700

    feat: add create for zoom_creadentials

---
+++ database/migrations/2022_02_16_135336_create_zoom_credentials_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('zoom_credentials', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sdk_key');
+            $table->string('sdk_secret');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('zoom_credentials');
+    }
+};
