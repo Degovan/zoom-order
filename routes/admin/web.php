2f0085c9512fa9c9529f737556d0967d6425ad52
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2022-02-18 07:43:56 +0700

    Revert "Merge pull request #2 from Degovan/zoom_credentials"
    This reverts commit 12ccebe2f091dfe3667f23c710c12232e2d2ce75, reversing
    changes made to 3cd2c39d579eaee687a8c9f9c40b04bbeddf7681.

--- routes/admin/web.php
+++ routes/admin/web.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Http\Controllers\Admin\{DashboardController,ZoomCredentialController};
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:admin'])->group(function() {
     Route::get('/', DashboardController::class)->name('dashboard');
-    Route::resource('/zoom-credentials', ZoomCredentialController::class);
 });
 
 include __DIR__ . '/auth.php';
