cd573ccb08316e25d5aa84ff7aababdb6da4e414
destroylord <masapin68@gmail.com>
2022-02-23 08:19:44 +0700

    feat: add featur invoice for admin

--- resources/views/layouts/admin/nav.blade.php
+++ resources/views/layouts/admin/nav.blade.php
@@ -18,14 +18,22 @@
                 </a>
             </div>
         </div>
-        
+
         <ul class="nav flex-column pt-3 pt-md-0">
             <li class="nav-item @active('admin.dashboard')">
                 <a href="{{ route('admin.dashboard') }}" class="nav-link">
                     <span class="sidebar-icon">
                         <i class="fas fa-fire"></i>
-                    </span> 
+                    </span>
                     <span class="sidebar-text">Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item @active('admin.invoice.index')">
+                <a href="{{route('admin.invoice.index')}}" class="nav-link">
+                    <span class="sidebar-icon">
+                        <i class="fas fa-fire"></i>
+                    </span>
+                    <span class="sidebar-text">invoice</span>
                 </a>
             </li>
         </ul>
--- routes/admin/web.php
+++ routes/admin/web.php
@@ -1,15 +1,15 @@
 <?php
 
 
-use App\Http\Controllers\Admin\{DashboardController, ProfileController, ZoomCredentialController};
-
+use App\Http\Controllers\Admin\{DashboardController, ProfileController, InvoiceController};
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:admin'])->group(function() {
     Route::get('/', DashboardController::class)->name('dashboard');
 
-    Route::resource('/zoom-credentials', ZoomCredentialController::class);
-    // Route::resource('/profile', ProfileController::class);
+    // Route::resource('/zoom-credentials', ZoomCredentialController::class);
+    Route::resource('/invoice', InvoiceController::class);
+
     // Profile
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
     Route::patch('/update-profile',[ ProfileController::class, 'update'])->name('profile.update');
