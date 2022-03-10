8b06094c8ce8897042b7d327e5f4dd9034cef5dc
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2022-03-10 15:31:48 +0700

    change: pricing table
    Chunk pricing to different table

---
+++ database/migrations/2022_03_10_081030_create_pricings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pricings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('package_id')->constrained()->onDelete('cascade');
+            $table->integer('max_audience');
+            $table->integer('cost');
+            $table->integer('discount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pricings');
+    }
+};
