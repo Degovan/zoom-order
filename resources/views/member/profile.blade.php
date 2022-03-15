8e36fe0fa5eca1cc4ca27053056c7b93e7a17631
sahrullahh <mohammadsahrullah894@gmail.com>
2022-03-15 16:23:32 +0700

    done select option

--- resources/views/member/profile.blade.php
+++ resources/views/member/profile.blade.php
@@ -58,7 +58,7 @@
                             <div class="form-group">
                                 <label>Provinsi</label>
                                 <div class="form-group">
-                                    <select class="choices form-select" name="province" onchange="getVal(this)">
+                                    <select class="choices form-select" id="provinsi" name="province">
                                         @foreach($provinces as $prov)
                                         <option value="{{ $prov->id }}">{{ $prov->name }}</option>
                                         @endforeach
@@ -70,7 +70,7 @@
                             <div class="form-group">
                                 <label>Kabupaten</label>
                                 <div class="form-group">
-                                    <select class="choices form-select" id="district">
+                                    <select class="form-select" id="district">
                                     </select>
                                 </div>
                             </div>
@@ -79,7 +79,7 @@
                             <div class="form-group">
                                 <label>Kecamatan</label>
                                 <div class="form-group">
-                                    <select class="choices form-select" id="sub_district">
+                                    <select class="form-select" id="sub_district">
                                     </select>
                                 </div>
                             </div>
@@ -105,85 +105,6 @@
                                         {{$message}}
                                     </div>
                                 @enderror
-<<<<<<< HEAD
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-group">
-                                <label for="email-id-vertical">Email</label>
-                                <input type="email" id="email-id-vertical" class="form-control" name="email" value="{{Auth::user()->email}}" placeholder="Email" readonly>
-                                </div>
-                            </div>
-                            <div class="col-4">
-                                <div class="form-group">
-                                    <label>Provinsi</label>
-                                    <div class="form-group">
-                                        <select class="choices form-select" id="provinsi">
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-4">
-                                <div class="form-group">
-                                    <label for="">Kabupaten</label>
-                                    <div class="form-group">
-                                        <select class="choices form-select" id="kabupaten">
-                                            <option value="square">Square</option>
-                                            <option value="rectangle">Rectangle</option>
-                                            <option value="rombo">Rombo</option>
-                                            <option value="romboid">Romboid</option>
-                                            <option value="trapeze">Trapeze</option>
-                                            <option value="traible">Triangle</option>
-                                            <option value="polygon">Polygon</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-4">
-                                <div class="form-group">
-                                    <label for="">Kecamatan</label>
-                                    <div class="form-group">
-                                        <select class="choices form-select" id="kecamatan">
-                                            <option value="square">Square</option>
-                                            <option value="rectangle">Rectangle</option>
-                                            <option value="rombo">Rombo</option>
-                                            <option value="romboid">Romboid</option>
-                                            <option value="trapeze">Trapeze</option>
-                                            <option value="traible">Triangle</option>
-                                            <option value="polygon">Polygon</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label>Lembaga/Institusi</label>
-                                    <input type="text" class="form-control @error('institution') is-invalid @enderror" value="{{Auth::user()->institution}}">
-                                    @error('institution')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label>No Handphone</label>
-                                    <input type="number" class="form-control @error('phone') is-invalid @enderror" value="{{Auth::user()->phone}}">
-
-                                    @error('phone')
-                                        <div class="invalid-feedback">
-                                            {{$message}}
-                                        </div>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="col-12 d-flex justify-content-end">
-                                <button type="submit" class="btn btn-primary me-1 mb-1">Submit</button>
-                                <button type="reset" class="btn btn-light-secondary me-1 mb-1">Reset</button>
-                            </div>
-=======
->>>>>>> e84fd91e7f93f0dd4cce4f4b133b41f4f3a75341
                             </div>
                         </div>
                         <div class="col-12 d-flex justify-content-end">
@@ -203,37 +124,50 @@
 @endsection
 
 @push('script')
-<<<<<<< HEAD
         <!-- Include Choices JavaScript -->
         <script src="/vendor/choices.js/choices.min.js"></script>
         <script>
-            let endpoint = "http://127.0.0.1:8000/api/region/");
-
-            $("#provinsi").on('change', function(){
-                $.ajax({
-                    type: 'GET',
-                    url: endpoint + 'provinces',
-                    dataType: 'json',
-                    success: function(data){
-                        $.each(data, function(index, item){
-                            $.each(item, function(key, value){
-                                $("#provinsi").append('<option>'+ value. +'</option>')
-                            }
-                        })
+        
+        const provinsi = document.querySelector("#provinsi");
+        const districts = document.querySelector("#district")
+        const sub_district = document.querySelector("#sub_district");
+        const choicesDistrict = new Choices(districts);
+        const choicesSubDistrict = new Choices(sub_district);
+        // console.log(provinsi)
+        
+        provinsi.addEventListener('change', function(){
+            fetch(`/api/region/districts/${this.value}`)
+            .then(res => res.json())
+            .then(data => {
+                choicesSubDistrict.clearStore();
+                choicesDistrict.clearStore();
+                choicesDistrict.setChoices(data.map(district => {
+                    return {
+                       label: district.name, 
+                       value: district.id
                     }
-                })
+                }));
+                
+                
             })
-        </script>
-=======
-<!-- Include Choices JavaScript -->
-<script src="/vendor/choices.js/choices.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+        })
 
-<script>
-    function getVal(sel) {
-        province_id = sel.value
-        console.log(province_id)
-    }
-</script>
->>>>>>> e84fd91e7f93f0dd4cce4f4b133b41f4f3a75341
+        districts.addEventListener('change', function(){
+            fetch(`/api/region/sub-districts/${this.value}`)
+            .then(res => res.json())
+            .then(data => {
+                choicesSubDistrict.clearStore();
+                choicesSubDistrict.setChoices(data.map(subdistrict => {
+                    return {
+                       label: subdistrict.name, 
+                       value: subdistrict.id
+                    }
+                }));
+                
+                
+            })
+        })
+        
+        
+        </script>
 @endpush
