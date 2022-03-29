3883671d7cb9d4bfe8630a84b684d26e40463408
destroylord <masapin68@gmail.com>
2022-03-29 23:33:51 +0700

    feat: authentication registration for member area

--- app/Http/Controllers/Member/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Member/Auth/RegisteredUserController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use Spatie\Permission\Models\Role;
 
 class RegisteredUserController extends Controller
 {
@@ -20,7 +21,7 @@ class RegisteredUserController extends Controller
      */
     public function create()
     {
-        return view('auth.register');
+        return view('member.auth.register');
     }
 
     /**
@@ -44,6 +45,8 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $user->assignRole('user');
 
         event(new Registered($user));
 
