db83bc7ec665176e133c2e6e6ce7c7b53da50be1
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2022-05-19 19:13:24 +0700

    add: last 10 orders detail to member dashboard

--- app/Http/Controllers/Member/DashboardController.php
+++ app/Http/Controllers/Member/DashboardController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
+use App\Models\Invoice;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function __invoke()
     {
-        return view('member.dashboard');
+        $lastOrders = Invoice::latest()
+                            ->where('status', 'complete')
+                            ->where('user_id', Auth::user()->id)
+                            ->limit(10)
+                            ->get();
+
+        return view('member.dashboard', compact('lastOrders'));
     }
 }
