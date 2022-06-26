2369ec98032161e82a1b6f6eca1c9c76f6158f24
sahrullahh <mohammadsahrullah894@gmail.com>
2022-06-26 16:28:53 +0700

    change: invoice page to new version

--- resources/views/member/invoice/detail.blade.php
+++ resources/views/member/invoice/detail.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.member.app')
 
 @push('style')
-    <link rel="stylesheet" href="/vendor/simple-datatables/style.css">
-    <link rel="stylesheet" href="/vendor/purescss/degov.css">
+<link rel="stylesheet" href="/vendor/simple-datatables/style.css">
+<link rel="stylesheet" href="/vendor/purescss/degov.css">
 @endpush
 
 @section('content')
@@ -12,75 +12,99 @@ $total = ($invoice->items->cost - $invoice->items->discount);
 <div class="main-content container-fluid">
     <div class="row">
         <div class="col-md-9">
-            <div class="card shadow-none border">
+            <div class="card w-100 ">
                 <div class="card-body">
-                    <div class="row justify-content-between">
-                        <div class="col-md-7">
-                            <h1>Invoice #{{ $invoice->code }} <span class="badge" id="invoice-status">{{ $invoice->status }}</span></h1>
-                        </div>
+                    <div class="row">
                         <div class="col-md-5">
-                            <div class="row">
-                                <div class="col-md-5">
-                                    <p class="fw-bold">Invoice date</p>
-                                </div>
-                                <div class="col-md-4">
-                                    <p>{{ $invoice->created_at->format('d/m/Y') }}</p>
-                                </div>
+                            <img src="{{asset('invoice/img/RapatVirtual.png')}}" class="logo-invoice"
+                                alt="RapatVirtual (Logo Horizontal)">
+                            <div class="mt-5">
+                                <p><b>PT. Furqan Digital Niaga</b></p>
+                                <p>Pondok Ungu Permai Block LL 1 No 26 Bekasi Utara, Kota Bekasi. 17125</p>
+                                <p>NPWP: 63.509.570.6-407.000 </p>
+                                <p>Telp. 0877-3249-8274</p>
                             </div>
-                            <div class="row mt-2">
-                                <div class="col-md-5">
-                                    <p class="fw-bold">Due Date</p>
-                                </div>
-                                <div class="col-md-5">
-                                    <p>{{ $invoice->due->diffForHumans() }}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row mt-4">
-                        <div class="col-md-7">
-                            <div class="text-wrapper">
-                                <h3>Pay to:</h3>
-                                <p class="mt-3">PT. Furqan digital indonesia</p>
-                                <p>NPWP : 00.000.000.0000.000</p>
-                                <p >Jl. Rowosari RT 04 RW 05. Meteseh, Kec Boja, Kabupaten Kendal, Jawa Tengah</p>
-                            </div>
-                        </div>
-                        <div class="col-md-5">
-                            <h3>Invoiced to : </h3>
-                            <p class="mt-3">{{ $invoice->user->institution }}</p>
-                            <p class="fw-normal">{{ $invoice->user->name }}</p>
-                            <p class="max-w-2xl ml-0">{{ $invoice->user->full_address }}</p>
-                        </div>
-                    </div>
+                            <div class="mb-5">
+                                <p class=" text-muted">Bill To:</p>
+                                <p><b>{{ $invoice->user->name }}</b></p>
+                                <p>{{ $invoice->user->institution }}</p>
+                                <p>{{ $invoice->user->full_address }}</p>
 
-                    <div class="table-wrapper mt-5">
-                        <h4 class="fw-bold">Invoice Item</h4>
-                        <table class="table mt-3">
+                            </div>
+                        </div>
+                        <div class="col-md-7 text-end ">
+                            <h1 class="font-normal">INVOICE</h1>
+                            <h5 class="font-normal text-muted "># inv{{ $invoice->code }}</h5>
+                            <span class="badge" id="invoice-status">{{ $invoice->status }}</span>
+                            <br />
+                            <table class="table mt-5 w-100 float-end table-borderless">
+                                <tbody>
+                                    <tr>
+                                        <td>Date: </td>
+                                        <td>{{ $invoice->created_at->format('d/m/Y') }}</td>
+
+                                    </tr>
+                                    <tr>
+                                        <td>Due Date: </td>
+                                        <td>{{ $invoice->due->diffForHumans() }}</td>
+                                    </tr>
+                                    <tr class="table-light">
+
+                                        <td><b>Balance Due:</b></td>
+                                        <td><b> {{ $total }}</b></td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="mt-5">
+                        <table class="table table-borderless">
                             <thead>
-                                <td class="fw-bold">Decription</td>
-                                <td class="fw-bold">Amount</td>
+                                <tr class="table-dark">
+                                    <th scope="col">Item</th>
+                                    <th scope="col">Quantity</th>
+                                    <th scope="col" class="text-end">Rate</th>
+                                    <th scope="col" class="text-end">Amount</th>
+                                </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td>{{ $invoice->items->title }}</td>
-                                    <td>@money($total)</td>
+                                    <td> <b>{{ $invoice->items->title }}</b><br>
+                                        Pilih Kapasitas: {{ $invoice->items->max_audience }} Attendee
+                                    </td>
+                                    <td>{{ $invoice->items->packets }}</td>
+                                    <td class="text-end">IDR {{ $invoice->items->cost }}</td>
+                                    <td class="text-end">{{ $invoice->total }}</td>
                                 </tr>
-                                <tr>
-                                    <td>Paket</td>
-                                    <td>{{ $invoice->items->packets }} paket</td>
-                                </tr>
+
+
                             </tbody>
                         </table>
                     </div>
-
-                    <div class="row justify-content-end">
-                        <div class="col-md-8">
-                            <table class="table table-borderless">
+                    <div class="mt-5 row">
+                        <div class="col-md-5">
+                            <img src="{{ asset('/invoice/img/sign.jpeg')}}" class="signature mt-5" alt="" />
+                        </div>
+                        <div class="col-md-7 float-end">
+                            <table class="table mt-5 w-100 float-end table-borderless">
                                 <tbody>
                                     <tr>
-                                        <th scope="row" class="text-right">Total</th>
-                                        <th>@money($total * $invoice->items->packets)</th>
+                                        <td class="text-muted">Subtotal: </td>
+                                        <td class="text-end">IDR {{ $total * $invoice->items->packets }}</td>
+
+                                    </tr>
+                                    <tr>
+                                        <td class="text-muted">Tax(0%):</td>
+                                        <td class="text-end">IDR 0.00</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-muted">Total:</td>
+                                        <td class="text-end">IDR {{ $total * $invoice->items->packets }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-muted">Amount Paid:</td>
+                                        <td class="text-end">IDR {{ $total * $invoice->items->packets }}</td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -103,10 +127,13 @@ $total = ($invoice->items->cost - $invoice->items->discount);
             </div>
             <div class="mt-4">
                 <h4>Actions</h4>
-                <a href="{{ route('member.invoice.print', $invoice)}}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
-                    <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-                    <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-                    </svg>  Download</a>
+                <a href="{{ route('member.invoice.print', $invoice)}}"><svg xmlns="http://www.w3.org/2000/svg"
+                        width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                        <path
+                            d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                        <path
+                            d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+                    </svg> Download</a>
             </div>
         </div>
         @endif
@@ -118,7 +145,7 @@ $total = ($invoice->items->cost - $invoice->items->discount);
 @push('script')
 <script src="/vendor/simple-datatables/simple-datatables.js"></script>
 <script>
-const status = document.getElementById('invoice-status');
+    const status = document.getElementById('invoice-status');
 
 switch(status.innerText.toLowerCase()) {
     case 'unpaid':
